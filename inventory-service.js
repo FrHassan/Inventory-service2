e54d20a718d891992d69580bff10b247f7eed712
FrHassan <48347671+FrHassan@users.noreply.github.com>
2025-03-17 10:51:21 +0000

    change /health endpoint

--- inventory-service.js
+++ inventory-service.js
@@ -13,7 +13,7 @@ let inventory = {
 app.use(bodyParser.json());
 
 // Health check after update 2
-app.get('/health', (req, res) => res.status(200).send('OK'));
+app.get('/health', (req, res) => res.status(200).send('OK-sud'));
 
 
 
